20d902c3238a00328dcc2e9bad4c15a834a30169
8enrich <henriqueftcunha@gmail.com>
2024-03-25 14:41:57 -0300

    Classe Assento e Aviao criadas

---
+++ Assento.py
@@ -0,0 +1,9 @@
+from typing import Type
+from Aviao import Aviao
+
+class Assento:
+
+    def __init__(self, assento_id: str, aviao: Type[Aviao]) -> None:
+        self.__assento_id = assento_id
+        self.aviao = aviao
+        self.ocupado = False
