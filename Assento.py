6cd199b90989d45457b16f5418172dfb92b93434
8enrich <henriqueftcunha@gmail.com>
2024-03-25 15:40:41 -0300

    Classe Ticket criada

--- Assento.py
+++ Assento.py
@@ -4,6 +4,6 @@ from Aviao import Aviao
 class Assento:
 
     def __init__(self, assento_id: str, aviao: Type[Aviao]) -> None:
-        self.__assento_id = assento_id
+        self.assento_id = assento_id
         self.aviao = aviao
         self.ocupado =False
