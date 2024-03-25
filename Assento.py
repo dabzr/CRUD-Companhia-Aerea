1c70c30bc29683f75276a2e447ed60caf3100b2d
8enrich <henriqueftcunha@gmail.com>
2024-03-25 14:51:28 -0300

    desfazendo commit

--- Assento.py
+++
@@ -1,9 +0,0 @@
-from typing import Type
-from Aviao import Aviao
-
-class Assento:
-
-    def __init__(self, assento_id: str, aviao: Type[Aviao]) -> None:
-        self.__assento_id = assento_id
-        self.aviao = aviao
-        self.ocupado = False
