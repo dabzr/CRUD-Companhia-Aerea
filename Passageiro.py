a21e0adc5c83513a9d48f609109fbd09bea212cd
dylanabzr <dylanab@pm.me>
2024-03-25 14:59:35 -0300

    Adição da classe passageiro

---
+++ Passageiro.py
@@ -0,0 +1,8 @@
+class Passageiro():
+    def __init__(self, nome:str, usuario:str, senha:str):
+        self.nome = nome
+        self.usuario = usuario
+        self.senha = senha
+
+    def acessar_tickets():
+        pass
