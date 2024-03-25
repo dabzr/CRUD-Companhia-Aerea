08e997fa017953a8464b8fe067dba2ef89dd9203
dylanabzr <dylanab@pm.me>
2024-03-25 15:14:19 -0300

    Adição da classe usuário

--- Passageiro.py
+++ Passageiro.py
@@ -1,8 +1,11 @@
+from typing import Type
+from Usuario import Usuario 
+
 class Passageiro():
-    def __init__(self, nome:str, usuario:str, senha:str):
+    def __init__(self, nome:str, usuario:Type[Usuario]):
         self.nome = nome
         self.usuario = usuario
-        self.senha = senha
+        pass
 
     def acessar_tickets():
         pass
---
+++ Usuario.py
@@ -0,0 +1,18 @@
+class Usuario():
+    def __init__(self, user, senha):
+        self.user = user
+        self.senha = criptografar_senha(senha)
+
+    def criptografar_senha():
+        pass
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @senha.setter
+    def senha(self, value):
+        self._senha = criptografar_senha(value)
+
+def criar_usuario():
+    pass
