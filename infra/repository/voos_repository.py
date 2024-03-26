124d87a908850ff6df83c83874a4fb797e39ab19
dylanabzr <dylanab@pm.me>
2024-03-26 16:21:49 -0300

    Mais repositories

--- infra/repository/voos_repository.py
+++ infra/repository/voos_repository.py
@@ -1,6 +1,6 @@
 from infra.configs.connection import DBConnectionHandler
 from infra.entities.Voo import Voo
-frominfra.entities.Aviao import Aviao
+from infra.entities.Aviao import Aviao
 from infra.entities.Aeroporto import Aeroporto
 from typing import Type
 from datetime import datetime
@@ -8,12 +8,18 @@ from datetime import datetime
 class VooRepository:
     def select(self):
         with DBConnectionHandler() as db:
-            data = db.session.query(Voo).all()
+            data = db.session\
+                .query(Voo, Aeroporto)\
+                .with_entities(Voo.horario, Aeroporto.nome)\
+                .all()
             return data
 
     def insert(self, aviao:Type[Aviao], aeroporto_de_chegada:Type[Aeroporto], aeroporto_de_saida:Type[Aeroporto], horario:datetime):
         with DBConnectionHandler() as db:
-            data_insert = Voo(id_aviao=aviao.id, id_aeroporto_de_chegada=aeroporto_de_chegada.id, id_aeroporto_de_saida=aeroporto_de_saida.id, horario=horario)
+            data_insert = Voo(id_aviao=aviao.id, 
+                              id_aeroporto_de_chegada=aeroporto_de_chegada.id, 
+                              id_aeroporto_de_saida=aeroporto_de_saida.id, 
+                              horario=horario)
             db.session.add(data_insert)
             db.session.commit()
 
