1706f120be2d4340a91040c07959093f251edc31
8enrich <henriqueftcunha@gmail.com>
2024-03-27 13:59:40 -0300

    Update: Consertando erro de sintaxe

--- infra/entities/Ticket.py
+++ infra/entities/Ticket.py
@@ -13,6 +13,6 @@ class Ticket(Base):
     assento = relationship("Assento", back_populates("ticket"))
     id_voo = Column(Integer, ForeignKey('voo.id'))
     voo = relationship("Voo", back_populates("ticket"))
-    id_passageiro = Column(Integer, FOreignKey('passageiro.id'))
+    id_passageiro = Column(Integer, ForeignKey('passageiro.id'))
     passageiro = relationship("Passageiro", back_populates("ticket"))
 
