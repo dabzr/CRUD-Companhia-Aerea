317610e2fd7996aace1f50ce08fd2728fd31e537
dylanabzr <dylanab@pm.me>
2024-04-01 17:13:28 -0300

    minor changes

--- infra/repository/password.py
+++ infra/repository/password.py
@@ -4,4 +4,4 @@ def create_hash_password(password):
     salt = gensalt(rounds=10)
     bpassword = password.encode('utf-8')
     hashed_password = hashpw(bpassword, salt)
-    return hashed_password.decode('utf-8')
+    return hashed_password.decode('utf-8'), salt.decode('utf-8')
