6f001588284270161060e0458467b6ee4940f3bc
dylanabzr <dylanab@pm.me>
2024-04-02 14:40:22 -0300

    ebaaa ta rodando

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        name = request.form.get("name")
-        senha = request.form.get("input_password")
-        repo = UserRepository()
-        repo.insert(usuario, senha)
+        if request.form["submit_button"] == "register_button":
+            return render_template("register.html")
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    usuario = request.form.get("name")
+    senha = request.form.get("password")
+    confirmar_senha = request.form.get("confirm_password")
+    if senha == confirmar_senha:
+        repo = UserRepository()
+        repo.insert(usuario, senha)
+        return render_template("register.html")
+    return render_template("register.html", error_message="As senhas precisam ser iguais")
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
